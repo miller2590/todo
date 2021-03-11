846ca1dbc15292879056eb67bcb2cde85f0ea117
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-03-10 22:24:20 -0600

    Done For Sure

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import environ as environ
 from flask import Flask, render_template, redirect, url_for
 from models.forms import AddTask, TaskOptions
-from os import environ
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MY_LITTLE_SECRET'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from models.tables import Task, db
